355c2d8a2920252181d205c26daa00bd646be6f1
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-11-04 14:13:21 +0100

    Enhance Firefox settings with language packs and policies
    Added language packs and policies for Firefox configuration.

--- users/lm/programs/browsers.nix
+++ users/lm/programs/browsers.nix
@@ -7,7 +7,23 @@
   programs = {
     firefox = {
       enable = true;
-      profiles.${username} = {};
+      languagePacks = [ "pl" ];
+      policies = {
+        DisableTelemetry = true;
+
+        # Extensions
+        ExtensionSettings = let
+          moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+        in {
+          "*".installation_mode = "blocked";
+      
+          uBlock0@raymondhill.net" = {
+            install_url       = moz "ublock-origin";
+            installation_mode = "force_installed";
+            updates_disabled  = false;
+          };
+        };
+      };
     };
   };
 }
