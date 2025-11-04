a1ca37b1a62f0fb7570a394370c55c9fbfcd673e
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-11-04 14:15:18 +0100

    Configure GNOME dconf profiles for user settings
    Added user dconf profile settings for GNOME.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -4,9 +4,23 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-services.gnome.core-apps.enable = false;
+  services.gnome.core-apps.enable = false;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+            "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+          ];
+        };
+      };
+    }
+  ];
 
 }
