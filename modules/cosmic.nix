19ead0e67203a5659ff6375bb451cdd342ac3c0d
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-11-04 15:09:21 +0100

    Enable COSMIC_DATA_CONTROL and update Firefox settings
    Added session variable for COSMIC_DATA_CONTROL and updated Firefox preferences to disable libadwaita theming.

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -3,9 +3,6 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
-  services.gnome.core-apps.enable = false;
-  services.gnome.core-developer-tools.enable = false;
-  services.gnome.games.enable = false;
   # services.xserver.excludePackages = [ pkgs.xterm ];
 
   services.displayManager.autoLogin = {
@@ -13,8 +10,13 @@
     user = username;
   };
 
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
+  programs.firefox.preferences = {
+    # disable libadwaita theming for Firefox
+    "widget.gtk.libadwaita-colors.enabled" = false;
+  };
+
   environment.systemPackages = with pkgs; [
 
   ];
-
 }
