b258904003b9213cfadc98d666ba3314bba3bc7d
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-11-04 15:16:20 +0100

    Update system.nix

--- modules/system.nix
+++ modules/system.nix
@@ -5,6 +5,36 @@
   ...
 }: {
 
+  boot = {
+
+    plymouth = {
+      enable = true;
+      theme = "hexagon_hud";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "hexagon_hud" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+
+  };
+
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = false;
   users.users.root.hashedPassword = "!";
